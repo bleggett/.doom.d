532e93caeafe993cfd456c1d593c369e32b270de
Ben Leggett <benjamin.leggett1@t-mobile.com>
2020-06-10 14:51:01 -0400

    Reorg stuff

--- config.el
+++ config.el
@@ -3,6 +3,7 @@
 ;; Place your private configuration here
 
 
+;; BEGIN GENERAL EMACS CONFIG
 ;;Use custom font
 (setq doom-font (font-spec :family "Droid Sans Mono" :size 17))
 
@@ -49,11 +50,13 @@
 ;; Autosave dirty Org buffers when they are no longer visible
 (add-to-list 'focus-out-hook (lambda () (save-some-buffers t nil)))
 
+;;When I quit, don't ask me if I want to kill processes
+(setq confirm-kill-processes nil)
+
 ;; Format Go files on save
 ;; (add-hook! 'before-save-hook #'gofmt-before-save)
 
 ;; Time display in modeline settings
-
 (add-hook! display-time-mode
   (setq display-time-format "%l:%M %p %b %e")
   (setq display-time-default-load-average nil)
@@ -61,6 +64,41 @@
 
 (display-time-mode 1)
 
+;; Delete to (freedesktop.org) trash
+(setq delete-by-moving-to-trash t)
+
+;; Indentation tweaks
+(setq-default indent-tabs-mode nil)
+(defun my-setup-indent (n)
+  ;;General tab size
+  (setq-default tab-width n)
+  ;; java/c/c++
+  (setq-default c-basic-offset n)
+  (setq-default go-tab-width n)
+  ;; web development
+  (setq-default coffee-tab-width n) ; coffeescript
+  (setq-default javascript-indent-level n) ; javascript-mode
+  (setq-default js-indent-level n) ; js-mode
+  (setq-default js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq-default web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq-default web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq-default web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq-default css-indent-offset n) ; css-mode
+  (setq-default typescript-indent-level n)
+
+  )
+(my-setup-indent 4)
+
+;; Define a custom Cloudformation minor mode that runs `cfn-lint'
+(define-derived-mode cfn-yaml-mode yaml-mode
+  "CFN-YAML"
+  "Simple mode to edit CloudFormation template in YAML format.")
+
+(add-to-list 'magic-mode-alist
+             '("\\(---\n\\)?AWSTemplateFormatVersion:" . cfn-yaml-mode))
+;; END GENERAL EMACS CONFIG
+
+;; BEGIN PACKAGE CONFIG
 ;; Tweak company-mode display options
 (after! company
   (custom-set-faces
@@ -70,9 +108,6 @@
      ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
   )
 
-;;When I quit, don't ask me if I want to kill processes
-(setq confirm-kill-processes nil)
-
 ;; Configure grip markdown previewer to pull Github API token from ~/.authinfo.gpg
 (use-package! grip-mode
   :defer
@@ -81,10 +116,38 @@
     (setq grip-github-user (car credential)
           grip-github-password (cadr credential)))
   )
-;; Delete to (freedesktop.org) trash
-(setq delete-by-moving-to-trash t)
 
-;; (setq +ivy-project-search-engines '(ag rg))
+(after! flycheck
+  ;; Set up cfn-lint integration if flycheck is installed
+  ;; Get flycheck here https://www.flycheck.org/
+  (flycheck-define-checker cfn-lint
+    "AWS CloudFormation linter using cfn-lint.
+
+  Install cfn-lint first: pip install cfn-lint
+
+  See `https://github.com/aws-cloudformation/cfn-python-lint'."
+
+    :command ("cfn-lint" "-f" "parseable" source)
+    :error-patterns ((warning line-start (file-name) ":" line ":" column
+                              ":" (one-or-more digit) ":" (one-or-more digit) ":"
+                              (id "W" (one-or-more digit)) ":" (message) line-end)
+                     (error line-start (file-name) ":" line ":" column
+                            ":" (one-or-more digit) ":" (one-or-more digit) ":"
+                            (id "E" (one-or-more digit)) ":" (message) line-end))
+    :modes (cfn-yaml-mode))
+
+  (add-to-list 'flycheck-checkers 'cfn-lint)
+  (add-hook! 'cfn-yaml-mode-hook 'flycheck-mode)
+
+
+  (setq flycheck-markdown-markdownlint-cli-config ".markdownlint.json")
+  )
+
+;; When I switch Counsel/Projectile projects, open Magit buffer by default
+(after! counsel-projectile
+  (counsel-projectile-modify-action 'counsel-projectile-switch-project-action '((default counsel-projectile-switch-project-action-vc)))
+  )
+;; END PACKAGE CONFIG
 
 ;; BEGIN Custom keybinds
 
@@ -132,16 +195,12 @@
 
 (use-package! org-journal
   :defer
-  :bind
-  ("C-c n j" . org-journal-new-entry)
-  :custom
-  (org-journal-dir "~/Dropbox/org-roam/")
-  (org-journal-date-prefix "#+TITLE: ")
-  (org-journal-file-format "%Y-%m-%d.org")
-  (org-journal-date-format "%A, %d %B %Y")
-  )
-
- (setq org-journal-enable-agenda-integration t)
+  :config
+  (setq org-journal-dir "~/Dropbox/org-roam/"
+        org-journal-date-prefix "#+TITLE: "
+        org-journal-file-format "%Y-%m-%d.org"
+        org-journal-date-format "%A, %d %B %Y"
+        org-journal-enable-agenda-integration t))
 
 (add-hook! org-roam-mode 'org-roam-server-mode)
 
@@ -192,6 +251,9 @@
         org-roam-server-label-truncate-length 60
         org-roam-server-label-wrap-length 20))
 
+;;END ORG-ROAM CONFIG
+
+
 ;;BEGIN ORGMODE CONFIG
 ;; Put orgmode config here, spacemacs does not use the built-in org version
 (after! org
@@ -204,15 +266,7 @@
   '(org-enforce-todo-dependencies t)
 
   ;; Tell org that we're using org-protocol
-  (add-to-list 'org-modules 'org-protocol)
-
-  ;;Syntax mode highight to ORG #+BEGIN_SRC code block mappings.
-  (add-to-list 'org-src-lang-modes (cons "JSON" 'json))
-  (add-to-list 'org-src-lang-modes (cons "CS" 'csharp))
-  (add-to-list 'org-src-lang-modes (cons "JS" 'js2))
-
-  ;; ;; Whenever I invoke org-agenda, it should open in another frame
-  (setq org-agenda-window-setup 'other-frame)
+  ;; (add-to-list 'org-modules 'org-protocol)
 
   ;;Org-babel graphing stuff
   (setq org-ditaa-jar-path "~/Dropbox/org/ditaa.jar")
@@ -226,10 +280,6 @@
         (org-display-inline-images)
       (error nil)))
 
-  ;; (org-babel-do-load-languages
-  ;;  (quote org-babel-load-languages)
-  ;;  (quote ((ditaa . t))))
-
   ;; Do not prompt to confirm evaluation
   ;; This may be dangerous - make sure you understand the consequences
   ;; of setting this -- see the docstring for details
@@ -238,50 +288,15 @@
   ;; Bullet list symbols
   (setq-default org-bullets-bullet-list '("•" "◊" "Δ" "†"))
 
-  ;; ;; Capture templates for: TODO tasks, Notes, and org-protocol
-  ;; (setq org-capture-templates
-  ;;       (quote (("t" "todo" entry (file org-default-notes-file)
-  ;;                "* TODO %?\n%U\n%a\n")
-  ;;               ("n" "note" entry (file org-default-notes-file)
-  ;;                "* %? :NOTE:\n%U\n%a\n")
-  ;;               ("p" "Protocol" entry (file org-default-notes-file)
-  ;;                "* %^{Title}\nSource: %:link\nAt: %u\n#+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?")
-  ;;               ("L" "Protocol Link" entry (file org-default-notes-file)
-  ;;                "* [[%:link][%:description]] :NOTE:\nCaptured On: %U" :immediate-finish t))))
 
   (setq org-todo-keywords
         (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
                 (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
 
-  ;; Targets include this file and any file contributing to the agenda - up to 9 levels deep
-  ;; (setq org-refile-targets (quote ((nil :maxlevel . 9) (org-agenda-files :maxlevel . 9))))
-
-  ;;Let me refile to top level headers
-  ;; (setq org-refile-use-outline-path 'file)
-  ;; (setq org-outline-path-complete-in-steps nil)
-
   ;; What files to include in the agenda
   ;; include subfolders, and archives for search purposes
   (let ((default-directory org-directory))
     (setq org-agenda-files (list org-directory (expand-file-name "conferences") (expand-file-name "archive"))))
-
-  ;; Enable org-super-agenda
-  (org-super-agenda-mode 1)
-
-  ;; ;; Since we use multiple blocks to 'fake' a complete agenda, drop separator
-  (setq org-agenda-block-separator nil)
-
-  ;; ;; Org super agenda headers have their own keymap - I want it to clone the `evil-org-agenda' map
-  (setq org-super-agenda-header-map nil)
-
-  ;; ;;Don't let children inherit tags
-  (setq org-use-tag-inheritance nil)
-
-  ;; ;; I want refile.org's file-level :REFILE: to be inherited, but only by the top-level heading
-  ;; (setq org-tags-match-list-sublevels nil)
-
-  ;; ;; Whenever I invoke org-agenda, it should open in another frame
-  ;; (setq org-agenda-window-setup 'other-frame)
 
   ;; ;; Custom agenda command definitions
   (setq org-agenda-custom-commands
@@ -305,66 +320,14 @@
           )
         )
 
-  ) ;; END ORGMODE CONFIG
-
-;; Indentation tweaks
-(setq-default indent-tabs-mode nil)
-(defun my-setup-indent (n)
-  ;;General tab size
-  (setq-default tab-width n)
-  ;; java/c/c++
-  (setq-default c-basic-offset n)
-  (setq-default go-tab-width n)
-  ;; web development
-  (setq-default coffee-tab-width n) ; coffeescript
-  (setq-default javascript-indent-level n) ; javascript-mode
-  (setq-default js-indent-level n) ; js-mode
-  (setq-default js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
-  (setq-default web-mode-markup-indent-offset n) ; web-mode, html tag in html file
-  (setq-default web-mode-css-indent-offset n) ; web-mode, css in html file
-  (setq-default web-mode-code-indent-offset n) ; web-mode, js code in html file
-  (setq-default css-indent-offset n) ; css-mode
-  (setq-default typescript-indent-level n)
-
   )
-(my-setup-indent 4)
-
-;; Define a custom Cloudformation minor mode that runs `cfn-lint'
-(define-derived-mode cfn-yaml-mode yaml-mode
-  "CFN-YAML"
-  "Simple mode to edit CloudFormation template in YAML format.")
-
-(add-to-list 'magic-mode-alist
-             '("\\(---\n\\)?AWSTemplateFormatVersion:" . cfn-yaml-mode))
-
-
-(after! flycheck
-  ;; Set up cfn-lint integration if flycheck is installed
-  ;; Get flycheck here https://www.flycheck.org/
-  (flycheck-define-checker cfn-lint
-    "AWS CloudFormation linter using cfn-lint.
-
-  Install cfn-lint first: pip install cfn-lint
-
-  See `https://github.com/aws-cloudformation/cfn-python-lint'."
-
-    :command ("cfn-lint" "-f" "parseable" source)
-    :error-patterns ((warning line-start (file-name) ":" line ":" column
-                              ":" (one-or-more digit) ":" (one-or-more digit) ":"
-                              (id "W" (one-or-more digit)) ":" (message) line-end)
-                     (error line-start (file-name) ":" line ":" column
-                            ":" (one-or-more digit) ":" (one-or-more digit) ":"
-                            (id "E" (one-or-more digit)) ":" (message) line-end))
-    :modes (cfn-yaml-mode))
-
-  (add-to-list 'flycheck-checkers 'cfn-lint)
-  (add-hook! 'cfn-yaml-mode-hook 'flycheck-mode)
-
-
-  (setq flycheck-markdown-markdownlint-cli-config ".markdownlint.json")
-  )
-
-;; When I switch Counsel/Projectile projects, open Magit buffer by default
-(after! counsel-projectile
-  (counsel-projectile-modify-action 'counsel-projectile-switch-project-action '((default counsel-projectile-switch-project-action-vc)))
-  )
+(use-package! org-super-agenda
+  :hook (org-agenda-mode . org-super-agenda-mode)
+  :config
+  ;; Since we use multiple blocks to 'fake' a complete agenda, drop separator
+  (setq org-agenda-block-separator nil
+        ;; Org super agenda headers have their own keymap - I want it to clone the `evil-org-agenda' map
+        org-super-agenda-header-map nil
+        ;;Don't let children inherit tags
+        org-use-tag-inheritance nil))
+;; END ORGMODE CONFIG
