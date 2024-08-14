b1ba99f84d202684deb89a1e829127dc9983ac84
Benjamin Leggett <benjamin.leggett@solo.io>
2024-08-14 00:47:43 -0400

    Okay fine
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

--- config.el
+++ config.el
@@ -10,6 +10,15 @@
 ;; No quit prompt please
 (setq confirm-kill-processes nil)
 
+;; override highly unprofessional, unsuitable-for-work-computer comic quit prompts with something
+;; proper and highly suitable
+(setq +doom-quit-messages
+      '("Oh you're trying to QUIT Emacs? Surely, that's accidental?"))
+
+;; Attempt to tame rust-lsp lag
+(setq gc-cons-threshold 200000000)
+(setq read-process-output-max (* 2048 2048))
+
 ;;Tighten which-key interval
 (setq which-key-idle-delay 0.4)
 
@@ -138,10 +147,10 @@
 ;;
 (add-hook! 'js2-mode-hook 'prettier-js-mode)
 
-;;Rust mode hook
-(add-hook! rust-mode
-  (setq rustic-lsp-server 'rust-analyzer)
-  )
+;; ;;Rust mode hook
+;; (add-hook! rust-mode
+;;   (setq rustic-lsp-server 'rust-analyzer)
+;;   )
 ;; Prefer SVG mermaid diagrams
 (setq mermaid-output-format ".svg")
 
@@ -505,3 +514,36 @@
 
 ;; Quell annoying vterm cursor thing with evil
 (defadvice! +vterm-update-cursor (orig-fn &rest args) :before #'vterm-send-key (vterm-goto-char (point)))
+
+
+;; Try to work around emacs shitty single-threadedness with lsp-booster
+(defun lsp-booster--advice-json-parse (old-fn &rest args)
+  "Try to parse bytecode instead of json."
+  (or
+   (when (equal (following-char) ?#)
+     (let ((bytecode (read (current-buffer))))
+       (when (byte-code-function-p bytecode)
+         (funcall bytecode))))
+   (apply old-fn args)))
+(advice-add (if (progn (require 'json)
+                       (fboundp 'json-parse-buffer))
+                'json-parse-buffer
+              'json-read)
+            :around
+            #'lsp-booster--advice-json-parse)
+
+(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+  "Prepend emacs-lsp-booster command to lsp CMD."
+  (let ((orig-result (funcall old-fn cmd test?)))
+    (if (and (not test?)                             ;; for check lsp-server-present?
+             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+             lsp-use-plists
+             (not (functionp 'json-rpc-connection))  ;; native json-rpc
+             (executable-find "emacs-lsp-booster"))
+        (progn
+          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+            (setcar orig-result command-from-exec-path))
+          (message "Using emacs-lsp-booster for %s!" orig-result)
+          (cons "emacs-lsp-booster" orig-result))
+      orig-result)))
+(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
--- init.el
+++ init.el
@@ -22,23 +22,24 @@
 
        :completion
        company           ; the ultimate code completion backend
+       ;;(corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;vertico
-       (ivy               ; a search engine for love and life
-        +prescient
-	+all-the-icons-ivy)
+       (vertico
+         +icons)
+       ;; (ivy               ; a search engine for love and life
+       ;;  +prescient
+       ;;  +all-the-icons-ivy)
         ;; +childframe)
 
        :ui
        deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        (emoji 
        	+unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
@@ -110,15 +111,13 @@
        ;;pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
-       ;;taskrunner        ; taskrunner for all your projects
        terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
@@ -151,8 +150,8 @@
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       (json              ; At least it ain't XML
-        +lsp)
+       json              ; At least it ain't XML
+        ;;+tree-sitter)
        (java
         +lsp) ; the poster child for carpal tunnel syndrome
        (javascript        ; all(hope(abandon(ye(who(enter(here))))))
@@ -202,8 +201,9 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        (web               ; the tubes
         +lsp)
-       (yaml              ; JSON, but readable
-        +lsp)
+       yaml              ; JSON, but readable
+        ;; +tree-sitter)
+       ;;zig               ; C, but simpler
 
        :email
        ;;(mu4e +org +gmail)
@@ -216,7 +216,6 @@
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
-       ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
        ;;literate
